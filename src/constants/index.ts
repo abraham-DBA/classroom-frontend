de9c0b473a5ec7493299af64317cb84b18dab38d
abraham-DBA <irootezekiel318@gmail.com>
2026-01-21 22:17:50 -0800

    implement data provider

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export const DEPARTMENTS = [
+    'Computer Science', 'Mathematics', 'Physics', 'Chemistry', 'Biology'
+]
+
+export const DEPARTMENT_OPTIONS = DEPARTMENTS.map((dept) => ({
+    value: dept,
+    label: dept,
+}))
